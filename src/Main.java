d96331dc60006a30364eb9b4eee0693244a6df0f
Andrés <andrescalvo01@gmail.com>
2025-07-01 12:23:18 +0200

    commit 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        Smartphone meuSmartphone= new Smartphone("Samsung", "Galaxy");
+
+       meuSmartphone.trucar("685665424");
+       meuSmartphone.fotografiar();
+       meuSmartphone.alarma();
+
+
+        }
+}
---
+++ src/Telefon.java
@@ -0,0 +1,12 @@
+public class Telefon {
+    private String marca;
+    private String model;
+
+    public Telefon(String marca, String model) {
+        this.marca = marca;
+        this.model = model;
+    }
+        public void trucar(String numero){
+            System.out.println("S´està trucant al numero: " + numero);
+    }
+}
